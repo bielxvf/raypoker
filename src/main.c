2135c0812a735c096e71dbbfd3cef2775c104129
Biel <bielxvf@gmail.com>
2025-02-11 15:33:13 +0100

    Button struct

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <dirent.h>
 #include <assert.h>
+#include <stdbool.h>
 
 #include <raylib.h>
 
@@ -25,6 +26,50 @@ typedef struct {
     size_t capacity;
 } Entries;
 
+typedef struct {
+    const char* text;
+    Rectangle rec;
+    Color fg_color;
+    Color bg_color;
+} Button;
+
+Button Button_init(Font font, const char* text, Vector2 pos, Color bg_color, Color fg_color)
+{
+    Vector2 btn_dims = MeasureTextEx(font, text, (float) font.baseSize, 1.0);
+    Button btn =
+    {
+        .text = text,
+        .rec = {
+            .width = btn_dims.x + 15.0,
+            .x = pos.x - (btn_dims.x + 15.0) / 2.0,
+            .height = btn_dims.y + 15.0,
+            .y = pos.y,
+        },
+        .bg_color = bg_color,
+        .fg_color = fg_color,
+    };
+
+    return btn;
+}
+
+void Button_draw(Font font, Button btn)
+{
+    DrawRectangleRec(btn.rec, btn.bg_color);
+    DrawTextEx(font, btn.text, (Vector2){ .x = btn.rec.x + 15.0/2.0, .y = btn.rec.y + 15.0/2.0 }, (float) font.baseSize, 1.0, btn.fg_color);
+}
+
+bool is_mouse_on_rec(Rectangle rec)
+{
+    Vector2 mouse_position = GetMousePosition();
+    if (mouse_position.x >= rec.x &&
+    mouse_position.x <= rec.x + rec.width &&
+    mouse_position.y >= rec.y &&
+    mouse_position.y <= rec.y + rec.height) {
+        return true;
+    }
+    return false;
+}
+
 int main()
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raypoker");
@@ -34,8 +79,8 @@ int main()
 
     const char* home = getenv("HOME");
     if (home == NULL) {
-	    fprintf(stderr, "No $HOME\n");
-	    return 1;
+        perror("No $HOME");
+	    abort();
     }
     DS config_dir = DS_from_cstr("");
     DS_push_back_cstr(&config_dir, home);
@@ -43,8 +88,9 @@ int main()
 
     DIR* dir = opendir(DS_to_cstr(&config_dir));
     if (dir == NULL) {
-        fprintf(stderr, "Couldn't open config_dir %s", DS_to_cstr(&config_dir));
-        return 1;
+        // @TODO Handle and create config dir
+        perror("Couldn't open config dir");
+        abort();
     }
 
     Entries entries =
@@ -69,33 +115,51 @@ int main()
     }
 
 
-    const char* btn_create_db_cstr = "Create Database";
-    Vector2 btn_create_db_vec2 = MeasureTextEx(font, btn_create_db_cstr, (float) font.baseSize, 1.0);
-
-    Rectangle btn_create_db_rec =
-    {
-        .width = btn_create_db_vec2.x + 15.0,
-        .x = (SCREEN_WIDTH - btn_create_db_vec2.x - 15.0)/2,
-        .height = btn_create_db_vec2.y + 15.0,
-        .y = (SCREEN_HEIGHT - btn_create_db_vec2.y - 15.0)/2,
-    };
+    Button btn_create_db = Button_init(font, "Create Database", (Vector2){ .x = SCREEN_WIDTH/2.0, .y = SCREEN_HEIGHT/2.0 }, RED, BLACK);
+    Button btn_start_session = Button_init(font, "Start Session", (Vector2){ .x = SCREEN_WIDTH/2.0, .y = SCREEN_HEIGHT/2.0 + 15.0 * 3 }, RED, BLACK);
+    Button btn_main_menu = Button_init(font, "Main Menu", (Vector2){ .x = SCREEN_WIDTH / 20.0, .y = SCREEN_WIDTH / 40.0 }, RED, BLACK);
 
     while (!WindowShouldClose()) {
         {
             switch (current_screen) {
             case MAIN_MENU: {
-                // Handle button presses
+                if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    if (is_mouse_on_rec(btn_create_db.rec)) {
+                        current_screen = CREATE_DB;
+                    } else if (is_mouse_on_rec(btn_start_session.rec)) {
+                        current_screen = START_SESSION;
+                    }
+                }
+            } break;
+            case CREATE_DB: {
+                if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    if (is_mouse_on_rec(btn_main_menu.rec)) {
+                        current_screen = MAIN_MENU;
+                    }
+                }
+            } break;
+            case START_SESSION: {
+                if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    if (is_mouse_on_rec(btn_main_menu.rec)) {
+                        current_screen = MAIN_MENU;
+                    }
+                }
             } break;
             }
-
         }
 	    BeginDrawing();
 	    {
 	        ClearBackground(WHITE);
             switch (current_screen) {
             case MAIN_MENU: {
-                DrawRectangleRec(btn_create_db_rec, RED);
-                DrawTextEx(font, btn_create_db_cstr, (Vector2){ .x = (SCREEN_WIDTH - btn_create_db_vec2.x)/2, .y = (SCREEN_HEIGHT - btn_create_db_vec2.y)/2 }, (float) font.baseSize, 1.0, BLACK);
+                Button_draw(font, btn_create_db);
+                Button_draw(font, btn_start_session);
+            } break;
+            case CREATE_DB: {
+                Button_draw(font, btn_main_menu);
+            } break;
+            case START_SESSION: {
+                Button_draw(font, btn_main_menu);
             } break;
             }
 	    }
